5186f38ff235f8f05cfa6603708f8292cbcda33a
Zelic Juarez <zelic.juamtz@gmail.com>
2019-08-12 01:21:38 -0500

    Creating Responsive SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from "react";
 import Toolbar from "../Navigation/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer";
 
 const Layout = props => (
   <Fragment>
     <Toolbar />
+    <SideDrawer />
     <main className="content">{props.children}</main>
   </Fragment>
 );
