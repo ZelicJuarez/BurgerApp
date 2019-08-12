43e3ae49c25ff462e8895d6d9ba61a064ef1ff89
Zelic Juarez <zelic.juamtz@gmail.com>
2019-08-12 16:35:49 -0500

    Improving Performance

--- src/components/Burger/OrderSummary.js
+++ src/components/Burger/OrderSummary.js
@@ -1,33 +1,43 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
 import Button from "../UI/Button";
 
-const OrderSummary = props => {
-  const summaryIngredients = Object.keys(props.ingredients).map(igKey => {
-    return (
-      <li key={igKey}>
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
-        {props.ingredients[igKey]}
-      </li>
-    );
-  });
+class OrderSummary extends Component {
+  // This could be a functional component, doesn't have to be a class
 
-  return (
-    <Fragment>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul style={{ paddingLeft: "3rem" }}>{summaryIngredients}</ul>
-      <p>
-        <strong>Total price: {props.price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to checkout?</p>
-      <Button btnType="danger" clicked={props.purchaseCancelled}>
-        CANCEL
-      </Button>
-      <Button btnType="success" clicked={props.purchaseContinued}>
-        CONTINUE
-      </Button>
-    </Fragment>
-  );
-};
+  componentWillUpdate() {
+    console.log("[OrderSummary] WillUpdate");
+  }
+
+  render() {
+    const summaryIngredients = Object.keys(this.props.ingredients).map(
+      igKey => {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
+            {this.props.ingredients[igKey]}
+          </li>
+        );
+      }
+    );
+
+    return (
+      <Fragment>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul style={{ paddingLeft: "3rem" }}>{summaryIngredients}</ul>
+        <p>
+          <strong>Total price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to checkout?</p>
+        <Button btnType="danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType="success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Fragment>
+    );
+  }
+}
 
 export default OrderSummary;
