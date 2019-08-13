bd8634dcfe739a849ab864090f14862127f8f515
Zelic Juarez <zelic.juamtz@gmail.com>
2019-08-12 23:22:39 -0500

    Removing Old Interceptors

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import Layout from "./hoc/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder";
 
-function App() {
-  return (
-    <div className="App">
-      <Layout>
-        <BurgerBuilder />
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  //TESTING - NOT SHOWING BURGERBUILDER AFTER 5SEC
+  /* state = {
+    show: true
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ show: false });
+    }, 5000);
+  } */
+
+  // <Layout>{this.state.show ? <BurgerBuilder /> : null}</Layout>
+
+  render() {
+    return (
+      <div className="App">
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/hoc/withErrorHandler.js
+++ src/hoc/withErrorHandler.js
@@ -9,16 +9,22 @@ const withErrorHandler = (WrappedComponent, axios) => {
 
     //we can use the constructor too
     componentWillMount() {
-      axios.interceptors.request.use(req => {
+      this.reqInterceptor = axios.interceptors.request.use(req => {
         this.setState({ error: null });
         return req;
       });
-      axios.interceptors.response.use(
+      this.resInterceptor = axios.interceptors.response.use(
         res => res,
         error => {
           this.setState({ error: error });
         }
       );
+    }
+
+    componentWillUnmount() {
+      //console.log("WillUnmount", this.reqInterceptor, this.resInterceptor);
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
     }
 
     errorConfirmedHandler = () => {
